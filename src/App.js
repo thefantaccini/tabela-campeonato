afb986b2e14e440fb58695aea6d8187f2c42134e
Vinícius Fantaccini <viniciusfantaccini@gmail.com>
2022-10-28 23:14:36 -0300

    criado ambiente para configuração dos times

--- src/App.js
+++ src/App.js
@@ -13,24 +13,53 @@ function App() {
         let data = await response.json()
         return data
     }
-
+    
     function setNewConfigs(e, config) {
-        const newValue = e.currentTarget.value
+        let newValue = e.currentTarget.value
         if (newValue < 0) newValue = 0
         let newConfig = JSON.parse(JSON.stringify(configs))
-        if (config == "numDeTimes") newConfig.numDeTimes = newValue
-        if (config == "numDeGrupos") newConfig.numDeGrupos = newValue
-        if (config == "classificadosPorGrupo") newConfig.classificadosPorGrupo = newValue
+        if (config == "numDeTimes") {
+            newConfig.numDeTimes = newValue
+            if (newConfig.numDeTimes > configs.numDeTimes) criarTime()
+            else removerTime()
+        }
+        if (config == "numDeGrupos") {
+            newConfig.numDeGrupos = newValue
+        }
+        if (config == "classificadosPorGrupo") {
+            newConfig.classificadosPorGrupo = newValue
+        }
         setConfigs(newConfig)
         //console.log(configs.numDeTimes)
     }
 
+    function criarTime() {
+        let novosTimes = JSON.parse(JSON.stringify(times))
+        novosTimes.push({
+            "id": times.length,
+            "nome": "Time" + (times.length + 1),
+            "grupo": 0,
+            "pontos": 0,
+            "vitorias": 0,
+            "empates": 0,
+            "derrotas": 0,
+            "gols-pro": 0,
+            "saldo": 0
+        })
+        setTimes(novosTimes)
+    }
+
+    function removerTime() {
+        let novosTimes = JSON.parse(JSON.stringify(times))
+        novosTimes.pop()
+        setTimes(novosTimes)
+    }
     function ConfigurarTabela(props) {
         return (
             <React.Fragment>
                 <label>Times: </label>
                 <input type='number'
-                value={configs.numDeTimes}
+                    value={configs.numDeTimes}
                     onChange={(e) => setNewConfigs(e,"numDeTimes")} />
                 <label>Grupos: </label>
                 <input type='number'
@@ -43,6 +72,15 @@ function App() {
             </React.Fragment>
                 )
     }
+    function ConfigurarTimes(props) {
+        console.log(times)
+        return (
+            times.map(t =>
+                <input type='text' value={t.nome} key={t.id} />
+                )
+        )
+
+    }
 
     useEffect(() => {
         getData().then(data => {
@@ -54,7 +92,11 @@ function App() {
 
 
     return (
-        <ConfigurarTabela/>
+        <React.Fragment>
+            <ConfigurarTabela />
+            <br/>
+            <ConfigurarTimes />
+        </React.Fragment>
       )
 }
 
